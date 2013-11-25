8888ec01bbae66d3d991e1378fcc2c5a2d02643e
TrevorDev <trevorjbaron@gmail.com>
2013-11-25 16:56:32 -0500

    Update installNodeJS.sh

--- installNodeJS.sh
+++ installNodeJS.sh
@@ -2,4 +2,5 @@ apt-get install curl;
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh;
 echo 'source ~/.nvm/nvm.sh' >> ~/.bashrc;
 source ~/.nvm/nvm.sh
-nvm;
+nvm install v0.10.15;
+nvm alias default v0.10.15;
